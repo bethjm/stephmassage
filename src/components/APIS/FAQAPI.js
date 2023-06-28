cf92d8a02c7c548fbe554386427a157dc7612941
bethjm <b.jeanne.mills@gmail.com>
2023-06-28 10:35:31 -0400

    added massage faq's

--- src/components/APIS/FAQAPI.js
+++ src/components/APIS/FAQAPI.js
@@ -1,54 +1,87 @@
 const FAQAPI = {
   general: [
     {
-      question: ".",
-      answer: ".",
+      question: `How should I dress for my session?`,
+      answer: `You can come in whatever you are comfortable with. During the massage you will be undressed to your comfort level(either naked or in underwear is typical). The areas I am not working on will be draped with a sheet and blanket.`,
     },
     {
-      question: ".",
-      answer: ".",
+      question: `Do I need to eat/drink before my session?`,
+      answer: `Coming into the massage feeling hydrated and consuming a light meal before will ensure you will be comfortable during the massage session.`,
+    },
+    {
+      question: `What if I don't want to talk during my session?`,
+      answer: `his is very common and not a problem at all. I will check in about comfort and pressure throughout the session but can be silent otherwise. I always defer to what the client wants from the experience!`,
     },
   ],
 
   massage: [
     {
-      question: ".",
-      answer: ".",
+      question: `What are the benefits of massage?`,
+      answer: `There are many benefits of massage including relaxation, stress reduction, injury prevention, increased range of motion, improved sleep, decrease of pain amongst others.`,
     },
     {
-      question: ".",
-      answer: ".",
+      question: `What ailments can your massage relieve?`,
+      answer: `Massage can help with muscle stiffness or soreness, pain, headaches, injury healing, TMJD, plantar fasciitis, sciatica.`,
+    },
+    {
+      question: `How are you different from other massage therapists?`,
+      answer: `I use a variety of techniques that I've studied blended into a session based on how the body is responding. I never charge an add on fee for different specialties. I am truly passionate about my work and feel it is my best compliment from my clients when they say they can feel how much I love what I do! I listen and adapt and use my gift of intuition and creativity in every single session.`,
+    },
+    {
+      question: `How do I know I need a massage?`,
+      answer: `If you are feeling stressed out, overwhelmed, tight, sore or like you’re not getting the most out of your workouts you may benefit from massage therapy.`,
+    },
+    {
+      question: `What type of clients do you typically see?`,
+      answer: `I see a range of clients for regular “tune up” massages. Typically late 20s-60s who either work 
+        Either at a desk or on their feet and stay active in their personal life.  My clients want to take care of their bodies and mind with regular massage. Clients come to me seeking preventative care to make sure they can live their best lives.`,
+    },
+    {
+      question: `How do you know what to work on during a massage?`,
+      answer: `I send an intake form via email prior to the session which will give me a good idea of your concerns, contraindications and goals. Before every session there is always time to check in and discuss what the main goals are for that session. Based off of this info I tailor the massage.`,
+    },
+    {
+      question: `How often should I get a massage?`,
+      answer: `It depends on your goals but typically a 4-6 period between massages is great for maintenance. If you are working on something specific every week for 5-6 weeks is helpful but would also recommend seeking other providers as well (ex: Physic Therapist, Chiropractic, acupuncturist, personal trainer)`,
+    },
+    {
+      question: `What if I don’t feel uncomfortable undressing?`,
+      answer: `We can work through the clothing using compression and stretching if you are uncomfortable undressing.`,
+    },
+    {
+      question: `What if there’s too much or not enough pressure during the massage?`,
+      answer: `I  will check in periodically but always encourage you to let me know at any point if you would prefer a different pressure.`,
     },
   ],
 
   reiki: [
     {
-      question: ".",
-      answer: ".",
+      question: `.`,
+      answer: `.`,
     },
     {
-      question: ".",
-      answer: ".",
+      question: `.`,
+      answer: `.`,
     },
   ],
   yoga: [
     {
-      question: ".",
-      answer: ".",
+      question: `.`,
+      answer: `.`,
     },
     {
-      question: ".",
-      answer: ".",
+      question: `.`,
+      answer: `.`,
     },
   ],
   cupping: [
     {
-      question: ".",
-      answer: ".",
+      question: `.`,
+      answer: `.`,
     },
     {
-      question: ".",
-      answer: ".",
+      question: `.`,
+      answer: `.`,
     },
   ],
 };
