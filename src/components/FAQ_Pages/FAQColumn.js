c8ee225df61159d1d0f83522dbeb6fe1ceacd727
bethjm <b.jeanne.mills@gmail.com>
2023-07-03 11:10:45 -0400

    added FAQ column and adjust to be mobile friendly

---
+++ src/components/FAQ_Pages/FAQColumn.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./FAQColumn.css";
+
+function FAQColumn() {
+  return (
+    <div className="faqcolumn">
+      <h1>Freqently Asked Questions</h1>
+      <h4>Still need help? Reach out to Steph</h4>
+    </div>
+  );
+}
+
+export default FAQColumn;
--- src/components/FAQ_Pages/GenFAQ.js
+++ src/components/FAQ_Pages/GenFAQ.js
@@ -6,26 +6,32 @@ import FAQAPI from "../APIS/FAQAPI";
 
 import Header from "../UI/Header";
 import Accordion from "../UI/Accordion/Accordion";
+import FAQColumn from "./FAQColumn";
 
 function FAQ() {
   return (
-    <div className="faq">
+    <>
       <Header />
-      <div className="faq_container">
-        <Accordion>
-          {FAQAPI.general.map(({ question, answer }) => {
-            return (
-              <Accordion.Item key={question}>
-                <Accordion.Collapsed id={question}>
-                  {question}
-                </Accordion.Collapsed>
-                <Accordion.Expanded>{answer}</Accordion.Expanded>
-              </Accordion.Item>
-            );
-          })}
-        </Accordion>
+      <div className="faq">
+        <div className="faq_container">
+          <FAQColumn />
+          <div className="faq_accordion">
+            <Accordion>
+              {FAQAPI.general.map(({ question, answer }) => {
+                return (
+                  <Accordion.Item key={question}>
+                    <Accordion.Collapsed id={question}>
+                      {question}
+                    </Accordion.Collapsed>
+                    <Accordion.Expanded>{answer}</Accordion.Expanded>
+                  </Accordion.Item>
+                );
+              })}
+            </Accordion>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
