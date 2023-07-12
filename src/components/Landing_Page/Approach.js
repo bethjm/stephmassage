7ac675a2950d3983274a5cdf1dd1f49e556acb0d
bethjm <b.jeanne.mills@gmail.com>
2023-07-12 10:10:40 -0400

    centered appraoch component

--- src/components/Landing_Page/Approach.js
+++ src/components/Landing_Page/Approach.js
@@ -5,36 +5,29 @@ import "./Approach.css";
 function Approach() {
   return (
     <div className="approach">
-      <div className="box">
-        <div className="approach_container">
-          <div className="approach_image"></div>
-          <div className="approach_text">
-            <div className="approach_upper_text">
-              <h2>My Approach</h2>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-                libero felis, porttitor sed est maximus, elementum luctus dolor.
-                Vestibulum imperdiet efficitur fermentum. Lorem ipsum dolor sit
-                amet, consectetur adipiscing elit. Donec libero felis, porttitor
-                sed est maximus, elementum luctus dolor. Vestibulum imperdiet
-                efficitur fermentum.
-              </p>
-            </div>
-            <div className="approach_lower_text">
-              <h2>My values</h2>
-              <ul>
-                <li>
-                  Fusce metus ligula, dapibus sit ametmalesuada vitae, sagittis
-                  nec ligula.
-                </li>
-                <li>
-                  Integer lobortis viverra nibh, vitae euismod eros porttitor
-                  ac.
-                </li>
-                <li>Sed pellentesque arcu sed felis </li>
-              </ul>
-            </div>
-          </div>
+      <div className="approach_container">
+        <div className="approach_image"></div>
+        <div className="approach_text">
+          <h2>My Approach</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            libero felis, porttitor sed est maximus, elementum luctus dolor.
+            Vestibulum imperdiet efficitur fermentum. Lorem ipsum dolor sit
+            amet, consectetur adipiscing elit. Donec libero felis, porttitor sed
+            est maximus, elementum luctus dolor. Vestibulum imperdiet efficitur
+            fermentum.
+          </p>
+          <h2>My values</h2>
+          <ul>
+            <li>
+              Fusce metus ligula, dapibus sit ametmalesuada vitae, sagittis nec
+              ligula.
+            </li>
+            <li>
+              Integer lobortis viverra nibh, vitae euismod eros porttitor ac.
+            </li>
+            <li>Sed pellentesque arcu sed felis </li>
+          </ul>
         </div>
       </div>
     </div>
