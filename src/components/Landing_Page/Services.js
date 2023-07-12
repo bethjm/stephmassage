653558bf15eb59ba58392a1e6af2beb32046ac19
bethjm <b.jeanne.mills@gmail.com>
2023-07-12 09:46:22 -0400

    centered services on landing page for mobile

--- src/components/Landing_Page/Services.js
+++ src/components/Landing_Page/Services.js
@@ -8,27 +8,25 @@ import reiki from "../images/reiki2.jpg";
 function Services() {
   return (
     <div className="services">
+      <h2>Services</h2>
       <div className="services_container">
-        <h2>Services</h2>
-        <div className="all_single_services">
-          <div className="single_service">
-            <a href="/massage">
-              <img src={massage} alt="person doing yoga" />
-              <h4>Massage</h4>
-            </a>
-          </div>
-          <div className="single_service">
-            <a href="/yoga">
-              <img src={yoga} alt="person doing yoga" />
-              <h4>Yoga</h4>
-            </a>
-          </div>
-          <div className="single_service">
-            <a href="/reiki">
-              <img src={reiki} alt="person doing yoga" />
-              <h4>Reiki</h4>
-            </a>
-          </div>
+        <div className="single_service">
+          <a href="/massage">
+            <img src={massage} alt="person doing yoga" />
+            <h4>Massage</h4>
+          </a>
+        </div>
+        <div className="single_service">
+          <a href="/yoga">
+            <img src={yoga} alt="person doing yoga" />
+            <h4>Yoga</h4>
+          </a>
+        </div>
+        <div className="single_service">
+          <a href="/reiki">
+            <img src={reiki} alt="person doing yoga" />
+            <h4>Reiki</h4>
+          </a>
         </div>
       </div>
     </div>
